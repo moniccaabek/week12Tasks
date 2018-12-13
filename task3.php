dd722747c3f535c5e87d3ea0a17ef0af42cf8377
Ulbolsyn Ibrashova <43200494+moniccaabek@users.noreply.github.com>
2018-12-13 21:25:44 +0600

    Create task3.php

---
+++ task3.php
@@ -0,0 +1,21 @@
+<?php
+
+    $id = $_GET["id"];
+
+    $basket = [];
+
+    if (isset($_COOKIE["basket"])){
+
+        $basket = json_decode($_COOKIE["basket"]);
+
+    }
+
+    array_push($basket,$id);
+
+    $expireTime = time() + 60*60*24;
+
+    setcookie("basket", json_encode($basket), $expireTime, '/week12Tasks');
+
+?>
+
+<a href='/week12/task3.php'>Back</a>
